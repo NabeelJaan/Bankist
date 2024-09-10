6028253bba0743cdb671a0a904ffc688ff29eb4c
Tayyab Rehman <47273004+NabeelJaan@users.noreply.github.com>
2024-09-11 00:27:25 +0500

    tabs component done

--- script.js
+++ script.js
@@ -30,6 +30,31 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
+/////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
+
+//Tabs Component
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function(e){
+  const btnClicked = e.target.closest('.operations__tab');
+  //Guard Clause
+  if (!btnClicked) return;
+  tabs.forEach( t => t.classList.remove('operations__tab--active'));
+  btnClicked.classList.add('operations__tab--active');
+
+  // Activate the content area
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+  const tabsContentArea = document.querySelector(`.operations__content--${btnClicked.dataset.tab}`).classList.add('operations__content--active');
+
+});
+
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
 const header = document.querySelector('.header');
 
 // Create Elements
@@ -40,15 +65,15 @@ message.classList.add('cookie-message');
 
 message.innerHTML = 'Agree. <button class="btn btn--close--cookie">Got it!</button>';
 
-
-// header.prepend(message);
 header.append(message);
 
-
 header.after(message);
-// header.after(message);
-
 
 document.querySelector('.btn--close--cookie').addEventListener( 'click', function(){
   message.remove();
 });
+
+message.style.backgroundColor = 'red';
+
+/////////////////////////////////////////////////////////////////
+
